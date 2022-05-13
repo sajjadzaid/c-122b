41c3e9d6d42fae875e2c89fda75a86514408ac99
sajjadzaid <92423630+sajjadzaid@users.noreply.github.com>
2022-05-13 14:02:16 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,58 @@
+x = 0;
+y = 0;
+
+draw_apple = "";
+screen_width="";
+screen_height="";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
+
+    to_number=Number(content);
+
+    if(Number.isInteger(to_number))
+}
+
+function setup() {
+ screen_width=window.innerWidth;
+ screen_height=window.innerHeight
+}
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    draw_apple = "";
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
+
+function setup(){
+  canvas=createCanvas(900,600);
+
+  canvas.position(0,150);
+}
